9f8924ad7fc87f44b731675e85cc76d1dde46314
auricom <27022259+auricom@users.noreply.github.com>
2024-05-12 19:08:38 +0200

    wip

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -3,28 +3,28 @@
 set -ouex pipefail
 
 # pre-enabled services
-systemctl enable cockpit.service
-systemctl enable node-exporter.service
-systemctl enable podman.socket
-systemctl enable podman-exporter.service
+# systemctl enable cockpit.service
+# systemctl enable node-exporter.service
+# systemctl enable podman.socket
+# systemctl enable podman-exporter.service
 
-if [[ -x "/usr/sbin/zpool" ]]; then
-    # ZFS
-    echo zfs > /etc/modules-load.d/zfs.conf
-    systemctl enable zrepl.service
+if [ -x "/usr/sbin/zpool" ]; then
+	# ZFS
+	echo zfs >/etc/modules-load.d/zfs.conf
+	systemctl enable zrepl.service
 
-    # NFS
-    # firewall-cmd --permanent --zone=FedoraServer --add-service=nfs
-    # firewall-cmd --permanent --zone=FedoraServer --add-service=rpc-bind
-    # firewall-cmd --permanent --zone=FedoraServer --add-service=mountd
-    systemctl enable nfs-server.service
+	# NFS
+	# firewall-cmd --permanent --zone=FedoraServer --add-service=nfs
+	# firewall-cmd --permanent --zone=FedoraServer --add-service=rpc-bind
+	# firewall-cmd --permanent --zone=FedoraServer --add-service=mountd
+	systemctl enable nfs-server.service
 
-    # Samba
-    # firewall-cmd --permanent --zone=FedoraServer --add-service=samba
-    systemctl enable smb.service
+	# Samba
+	# firewall-cmd --permanent --zone=FedoraServer --add-service=samba
+	systemctl enable smb.service
 
-    # Firewall / SELinux
-    restorecon -R /mnt/vol1
-    # firewall-cmd --reload
-    semanage fcontext --add --type "public_content_rw_t" "/mnt/vol1(/.*)?"
+	# Firewall / SELinux
+	restorecon -R /mnt/vol1
+	# firewall-cmd --reload
+	semanage fcontext --add --type "public_content_rw_t" "/mnt/vol1(/.*)?"
 fi
