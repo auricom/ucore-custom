c86142551b18351e05a2cc9617fa5d9f686249ad
auricom <27022259+auricom@users.noreply.github.com>
2024-05-14 00:02:10 +0200

    wip

--- scripts/apps/zrepl.sh
+++ scripts/apps/zrepl.sh
@@ -5,7 +5,7 @@ set -ouex pipefail
 # renovate: datasource=github-releases depName=zrepl/zrepl
 ZREPL_VERSION=v0.6.1
 
-RELEASE_INFO=$(curl -s "https://api.github.com/repos/zrepl/zrepl/releases/tags/$ZREPL_VERSION")
-ASSET_FILENAME=$(echo "$RELEASE_INFO" | grep -oP '"browser_download_url": "\K[^"]+' | grep 'x86_64\.rpm$')
+RELEASE_INFO=$(curl -s "https://api.github.com/repos/zrepl/zrepl/releases/tags/${ZREPL_VERSION}")
+ASSET_FILENAME=$(echo "${RELEASE_INFO}" | grep -oP '"browser_download_url": "\K[^"]+' | grep 'x86_64\.rpm$')
 
 rpm-ostree install "${ASSET_FILENAME}"
