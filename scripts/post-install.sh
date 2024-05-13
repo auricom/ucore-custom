f733ca3ad4b199779d264a7062cdda6dbb97a3e1
auricom <27022259+auricom@users.noreply.github.com>
2024-05-14 00:27:54 +0200

    wip

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -9,7 +9,10 @@ systemctl enable netavark-firewalld-reload.service
 if [[ -x "/usr/sbin/zpool" ]]; then
     # ZFS
     echo zfs > /etc/modules-load.d/zfs.conf
-    # systemctl enable zrepl.service
+    systemctl enable zrepl.service
+
+    # zrepl-exporter
+    # firewall-cmd --zone=FedoraServer --add-port=9811/tcp --permanent
 
     # NFS
     # firewall-cmd --permanent --zone=FedoraServer --add-service=nfs
@@ -21,10 +24,17 @@ if [[ -x "/usr/sbin/zpool" ]]; then
     # firewall-cmd --permanent --zone=FedoraServer --add-service=samba
     systemctl enable smb.service
 
-    # Firewall / SELinux
+    # SELinux
     # restorecon -R /mnt/vol1
-    # firewall-cmd --reload
     # semanage fcontext --add --type "public_content_rw_t" "/mnt/vol1(/.*)?"
     semodule --install /usr/share/minio/selinux.cil /usr/share/udica/templates/{base_container.cil,net_container.cil}
     semodule --install /usr/share/filebrowser/selinux.cil /usr/share/udica/templates/{base_container.cil,net_container.cil}
 fi
+
+# node-exporter
+# firewall-cmd --zone=FedoraServer --add-port=9100/tcp --permanent
+# podman-exporter
+# firewall-cmd --zone=FedoraServer --add-port=9882/tcp --permanent
+
+# fw reload
+# firewall-cmd --reload
