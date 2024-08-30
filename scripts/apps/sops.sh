06055c75ec10e5f39e2a3046157b940c36ae6b62
auricom <27022259+auricom@users.noreply.github.com>
2024-08-30 11:01:45 +0200

    fix: sops

--- scripts/apps/sops.sh
+++ scripts/apps/sops.sh
@@ -8,18 +8,23 @@ SOPS_VERSION=v3.9.0
 
 get_rpm_url() {
     local version=$1
-    local api_url="https://api.github.com/repos/getsops/sops/releases/tags/${version}"
+    local release_url="https://api.github.com/repos/getsops/sops/releases/tags/${version}"
 
-    rpm_url=$(curl -s "$api_url" | jq -r '.assets[] | select(.name | endswith(".x86_64.rpm")) | .browser_download_url')
+    rpm_url=$(curl -s "${release_url}" | grep -oP '(?<="browser_download_url": ")[^"]*\.x86_64\.rpm')
 
-    echo "$rpm_url"
+    if [[ -z "${rpm_url}" ]]; then
+        echo "No RPM found for SOPS version ${version}"
+        exit 1
+    fi
+
+    echo "${release_url}/${rpm_url}"
 }
 
-RPM_URL=$(get_rpm_url "$SOPS_VERSION")
+RPM_URL=$(get_rpm_url "${SOPS_VERSION}")
 
-if [ -z "$RPM_URL" ]; then
-    echo "No RPM found for SOPS version $SOPS_VERSION"
+if [[ -z "${RPM_URL}" ]]; then
+    echo "No RPM found for SOPS version ${SOPS_VERSION}"
     exit 1
 fi
 
-rpm-ostree install "$RPM_URL"
+rpm-ostree install "${RPM_URL}"
